5320a032b6a3ba995c7ccb8c280fb65078ef987d
synacktra.work@gmail.com <synacktra.work@gmail.com>
2025-04-06 20:35:42 +0530

    feat: add 'bot-follow-a-player' implementation

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# hacking-minecraft
+> Make sure you have [node.js](https://nodejs.org/en/download) installed.
+
+## Bots
+
+### bot-follow-a-player
+
+This bot will follow a (nearby) player with a given name.
+
+```
+Usage: index.js --player <name> [--interval <ms>] [--range <blocks>] [--bot
+<name>]
+
+Options:
+      --version   Show version number                                  [boolean]
+  -p, --player    Name of the player to follow               [string] [required]
+  -i, --interval  Interval in ms to check for the player [number] [default: 500]
+  -r, --range     Range in blocks to check for the player  [number] [default: 3]
+  -b, --bot       Name for the bot                     [string] [default: "bot"]
+  -h, --help      Show help                                            [boolean]
+```
+
+```bash
+cd bot-follow-a-player
+node index.js --player <name>
+```
+
+---
---
+++ bot-follow-a-player/index.js
@@ -0,0 +1,73 @@
+/**
+ * Script for creating a bot to follow a particular (nearby) player
+ * 
+ * Usage:
+ * node index.js --player <name> [--interval <ms>] [--range <blocks>] [--bot <name>]
+ */
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+// Parse command line arguments
+const argv = yargs(hideBin(process.argv))
+  .option('player', {
+    alias: 'p',
+    description: 'Name of the player to follow',
+    type: 'string',
+    demandOption: true
+  })
+  .option('interval', {
+    alias: 'i',
+    description: 'Interval in ms to check for the player',
+    type: 'number',
+    default: 500
+  })
+  .option('range', {
+    alias: 'r',
+    description: 'Range in blocks to check for the player',
+    type: 'number',
+    default: 3
+  })
+  .option('bot', {
+    alias: 'b',
+    description: 'Name for the bot',
+    type: 'string',
+    default: 'bot'
+  })
+  .usage('Usage: $0 --player <name> [--interval <ms>] [--range <blocks>] [--bot <name>]')
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+// Extract parameters from parsed arguments
+const playerName = argv.player;
+const interval = argv.interval;
+const range = argv.range;
+const botName = argv.bot;
+
+const mineflayer = require('mineflayer');
+const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder');
+
+const bot = mineflayer.createBot({
+  host: 'localhost',
+  username: botName,
+});
+
+bot.on('spawn', () => {
+  // Load the pathfinder plugin and set the movements
+  bot.loadPlugin(pathfinder);
+  bot.pathfinder.setMovements(new Movements(bot));
+  
+  bot.chat(`Hey, ${playerName}, I am here!`);
+
+  // Check for the player every {interval} ms
+  setInterval(() => {
+    // Find the nearest entity with the given name
+    const entity = bot.nearestEntity((e) => e.username === playerName);
+    if (entity !== null) {
+      // Set the goal to the player
+      const { x, y, z } = entity.position;
+      bot.pathfinder.setGoal(new GoalNear(x, y, z, range));
+    }
+  }, interval);
+});
